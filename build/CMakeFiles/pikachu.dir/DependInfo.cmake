ee7414da5e0fb425b3153cda7356cbe8f23bb7e8
Khang Lam <lvkhang16@gmail.com>
2025-01-02 18:15:29 +0700

    Added CMakeLists.txt

---
+++ build/CMakeFiles/pikachu.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/lvkha/Desktop/pikachu-matching-game/src/cell.cpp" "CMakeFiles/pikachu.dir/src/cell.cpp.obj" "gcc" "CMakeFiles/pikachu.dir/src/cell.cpp.obj.d"
+  "C:/Users/lvkha/Desktop/pikachu-matching-game/src/main.cpp" "CMakeFiles/pikachu.dir/src/main.cpp.obj" "gcc" "CMakeFiles/pikachu.dir/src/main.cpp.obj.d"
+  "C:/Users/lvkha/Desktop/pikachu-matching-game/src/menuAndControl.cpp" "CMakeFiles/pikachu.dir/src/menuAndControl.cpp.obj" "gcc" "CMakeFiles/pikachu.dir/src/menuAndControl.cpp.obj.d"
+  "C:/Users/lvkha/Desktop/pikachu-matching-game/src/normalGame.cpp" "CMakeFiles/pikachu.dir/src/normalGame.cpp.obj" "gcc" "CMakeFiles/pikachu.dir/src/normalGame.cpp.obj.d"
+  "C:/Users/lvkha/Desktop/pikachu-matching-game/src/slidingGame.cpp" "CMakeFiles/pikachu.dir/src/slidingGame.cpp.obj" "gcc" "CMakeFiles/pikachu.dir/src/slidingGame.cpp.obj.d"
+  "C:/Users/lvkha/Desktop/pikachu-matching-game/src/utility.cpp" "CMakeFiles/pikachu.dir/src/utility.cpp.obj" "gcc" "CMakeFiles/pikachu.dir/src/utility.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pikachu.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pikachu.dir/src/cell.cpp.obj"
+  "CMakeFiles/pikachu.dir/src/cell.cpp.obj.d"
+  "CMakeFiles/pikachu.dir/src/main.cpp.obj"
+  "CMakeFiles/pikachu.dir/src/main.cpp.obj.d"
+  "CMakeFiles/pikachu.dir/src/menuAndControl.cpp.obj"
+  "CMakeFiles/pikachu.dir/src/menuAndControl.cpp.obj.d"
+  "CMakeFiles/pikachu.dir/src/normalGame.cpp.obj"
+  "CMakeFiles/pikachu.dir/src/normalGame.cpp.obj.d"
+  "CMakeFiles/pikachu.dir/src/slidingGame.cpp.obj"
+  "CMakeFiles/pikachu.dir/src/slidingGame.cpp.obj.d"
+  "CMakeFiles/pikachu.dir/src/utility.cpp.obj"
+  "CMakeFiles/pikachu.dir/src/utility.cpp.obj.d"
+  "Debug/pikachu.exe"
+  "Debug/pikachu.exe.manifest"
+  "Debug/pikachu.pdb"
+  "libpikachu.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pikachu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
